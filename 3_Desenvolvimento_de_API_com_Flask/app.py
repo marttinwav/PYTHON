01a2f3be3093f9c89d25f6924ca8a47a724726d4
marttin <osmar.martinjunior123@gmail.com>
2025-04-09 16:30:28 -0300

    Adicionando testes e atualizando o endpoint de criação

--- 3_Desenvolvimento_de_API_com_Flask/app.py
+++ 3_Desenvolvimento_de_API_com_Flask/app.py
@@ -20,12 +20,12 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso!"})
-
+    return jsonify({"message": "Nova tarefa criada com sucesso!", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     task_list = [task.to_dict() for task in tasks]
+
     output = {
         "tasks": task_list,
         "total_tasks": len(task_list)
